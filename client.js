a51367af43d5c4246c424462c96977aa8a9a0782
Tanachi <tanachige@gmail.com>
2016-06-30 21:11:16 -1000

    able to get data from webpage

--- client.js
+++ client.js
@@ -1,13 +1,33 @@
 var net = require('net');
+var url = require('url');
+
 var Stream = require('stream');
 var WriteableStream = Stream.Writable;
 var ReadableStream = Stream.Readable;
 var socket = new net.Socket();
 var location = process.argv[2];
 
+var a = process.stdin;
+var b = process.stdout;
 if(location === undefined){
-  console.log('Please enter url: node client.js <url>');
+  console.log('please enter host and url node client.js <host>');
 }
 else{
-
+  socket.connect({port:80, host:location}, function(){
+    console.log('connection established');
+    var date = new Date();
+     socket.write('GET /apply HTTP/1.1\n' +
+     'Host: www.devleague.com\n' +
+     'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36\n' +
+     'Date:' + date.toUTCString() + '\n');
+     socket.write('\n\n');
+  });
 }
+
+
+socket.on('data', function(chunk){
+  console.log('data retrieved');
+  console.log(chunk.toString());
+  socket.end();
+  process.exit();
+});
